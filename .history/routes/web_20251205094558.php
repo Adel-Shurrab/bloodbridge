0cffbc8309b301b0b764990625d738fd8607a404
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2025-12-05 09:46:27 +0200

    register page

--- .history/routes/web_20251205094558.php
+++ .history/routes/web_20251205094558.php
@@ -9,7 +9,7 @@ Route::view('/contact', 'pages.contact')->name('contact');
 Route::view('/privacy-policy', 'pages.privacy')->name('privacy');
 
 // Registration Pages (We will create these views next)
-Route::view('/register-selection', 'auth.register-selection')->name('register.selection');
+Route::view('/registerselection', 'auth.register-selection')->name('register.selection');
 Route::view('/register/donor', 'auth.register-donor')->name('register.donor');
 Route::view('/register/organization', 'auth.register-organization')->name('register.organization');
 
