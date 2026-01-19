7c019825151c49284845abee246a5c0393b5ff77
Adel Shurrab <143008607+Adel-Shurrab@users.noreply.github.com>
2026-01-19 08:55:34 +0200

    delete

--- .history/debug_update_definition_20260116134807.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $reflector = new ReflectionFunction('update');
-    echo "Function 'update' found!\n";
-    echo "File: " . $reflector->getFileName() . "\n";
-    echo "Line: " . $reflector->getStartLine() . "\n";
-} catch (ReflectionException $e) {
-    echo "Function 'update' not found as a global function.\n";
-}
--- .history/test_parent_20260111140001.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-$r = new ReflectionClass('App\Filament\Pages\ManageGeneralSettings');
-echo "Parent: " . $r->getParentClass()->getName() . "\n";
-echo "File: " . $r->getParentClass()->getFileName() . "\n";
